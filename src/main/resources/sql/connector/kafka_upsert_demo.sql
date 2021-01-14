24251a6b8178430b3edf751022d79242de0fbace
venn <wxchunjhyy@163.com>
2021-01-14 17:15:54 +0800

    update myUpsertKafka to flink upsert-kafka

--- src/main/resources/sql/connector/kafka_upsert_demo.sql
+++ src/main/resources/sql/connector/kafka_upsert_demo.sql
@@ -6,42 +6,34 @@ CREATE TABLE user_log (
   ,behavior VARCHAR
   ,ts TIMESTAMP(3)
 ) WITH (
-  'connector.type' = 'kafka'
-  ,'connector.version' = 'universal'
-  ,'connector.topic' = 'user_behavior'                            -- required: topic name from which the table is read
-  ,'connector.properties.zookeeper.connect' = 'localhost:2181'    -- required: specify the ZooKeeper connection string
-  ,'connector.properties.bootstrap.servers' = 'localhost:9092'    -- required: specify the Kafka server connection string
-  ,'connector.properties.group.id' = 'user_log'                   -- optional: required in Kafka consumer, specify consumer group
-  ,'connector.startup-mode' = 'group-offsets'                     -- optional: valid modes are "earliest-offset", "latest-offset", "group-offsets",  "specific-offsets"
-  ,'connector.sink-partitioner' = 'fixed'                         --optional fixed 每个 flink 分区数据只发到 一个 kafka 分区
-                                                                          -- round-robin flink 分区轮询分配到 kafka 分区
-                                                                          -- custom 自定义分区策略
-  --,'connector.sink-partitioner-class' = 'org.mycompany.MyPartitioner'   -- 自定义分区类
-  ,'format.type' = 'json'                 -- required:  'csv', 'json' and 'avro'.
+  'connector' = 'kafka'
+  ,'topic' = 'user_behavior'
+  ,'properties.bootstrap.servers' = 'localhost:9092'
+  ,'properties.group.id' = 'user_log'
+  ,'scan.startup.mode' = 'group-offsets'
+  ,'format' = 'json'
 );
 
 -- kafka sink
 CREATE TABLE user_log_sink (
-  user_id VARCHAR
-  ,max_tx BIGINT
+  user_id varchar
+  ,max_tx bigint
+  ,primary key (user_id) not enforced
 ) WITH (
-  'connector.type' = 'upsertKafka'
-  ,'connector.version' = 'universal'
-  ,'connector.topic' = 'user_behavior_sink'                            -- required: topic name from which the table is read
-  ,'connector.properties.zookeeper.connect' = 'localhost:2181'    -- required: specify the ZooKeeper connection string
-  ,'connector.properties.bootstrap.servers' = 'localhost:9092'    -- required: specify the Kafka server connection string
-  ,'connector.properties.group.id' = 'user_log'                   -- optional: required in Kafka consumer, specify consumer group
-  ,'connector.startup-mode' = 'group-offsets'                     -- optional: valid modes are "earliest-offset", "latest-offset", "group-offsets",  "specific-offsets"
-  ,'connector.sink-partitioner' = 'fixed'                         --optional fixed 每个 flink 分区数据只发到 一个 kafka 分区
-                                                                          -- round-robin flink 分区轮询分配到 kafka 分区
-                                                                          -- custom 自定义分区策略
-  --,'connector.sink-partitioner-class' = 'org.mycompany.MyPartitioner'   -- 自定义分区类
-  ,'format.type' = 'json'                 -- required:  'csv', 'json' and 'avro'.
+  'connector' = 'upsert-kafka'
+  ,'topic' = 'user_behavior_sink'
+  ,'properties.bootstrap.servers' = 'localhost:9092'
+  ,'properties.group.id' = 'user_log'
+  ,'key.format' = 'json'
+  ,'key.json.ignore-parse-errors' = 'true'
+  ,'value.format' = 'json'
+  ,'value.json.fail-on-missing-field' = 'false'
+  ,'value.fields-include' = 'ALL'
+--   ,'format' = 'json'
 );
 
 -- insert
 insert into user_log_sink
 select user_id, count(user_id)
 from user_log
---group by TUMBLE(PROCTIME(), INTERVAL '10' SECOND ),user_id ;
 group by user_id ;
