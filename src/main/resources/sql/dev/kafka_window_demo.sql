5c0592309ebefa71ef40dcfb83b04d294f99c9e3
venn <wxchunjhyy@163.com>
2021-06-21 09:39:28 +0800

    update

--- src/main/resources/sql/dev/kafka_window_demo.sql
+++ src/main/resources/sql/dev/kafka_window_demo.sql
@@ -1,12 +1,13 @@
--- parse special json
+-- parse special json, then process window
 CREATE TABLE t_feature (
-  header STRING
+  header      ROW(`catalog` STRING, readTime bigint)
   ,readModule STRING
   ,checkPoint STRING
-  ,operation STRING
-  ,location ROW(id BIGINT, code STRING, send_time STRING, rms  decimal(12, 8),mean decimal(12, 8),peak decimal(12, 8),kurtosis decimal(12, 8),skewness decimal(12, 8))
-  ,data ROW(meta STRING, `rows` ARRAY<STRING>)
+  ,operation  STRING
+  ,location   ROW(id BIGINT, code STRING, send_time STRING, rms  decimal(12, 8),mean decimal(12, 8),peak decimal(12, 8),kurtosis decimal(12, 8),skewness decimal(12, 8))
+  ,data       ROW(meta STRING, `rows` ARRAY<STRING>)
   ,process_time as proctime()
+--   ,watermark for header.readTime as header.readTime - INTERVAL '5' SECOND -- not work
 ) WITH (
   'connector' = 'kafka'
   ,'topic' = 'test_dd'
@@ -28,16 +29,21 @@ SELECT operation
     ,cast(data.`rows`[8] as decimal(12, 8)) skewness
     ,location.code
     ,process_time
+--     ,read_time
 FROM t_feature;
 
 CREATE TABLE t_sink (
-    code        STRING
-    ,rms         decimal(12, 8)
+    code          STRING
+    ,window_start TIMESTAMP(3)
+    ,window_end   TIMESTAMP(3)
+    ,coun         BIGINT
+    ,rms          DECIMAL(12, 8)
 ) WITH (
    'connector' = 'print'
 );
 
 insert into t_sink
-SELECT code, rms min_rms FROM TABLE(
-   TUMBLE(TABLE v_t_feature, DESCRIPTOR(process_time), INTERVAL '1' MINUTES))
+SELECT code, window_start, window_end, count(id) coun, min(rms) min_rms FROM
+TABLE(TUMBLE(TABLE v_t_feature, DESCRIPTOR(process_time), INTERVAL '1' MINUTES))
+   GROUP BY code, window_start, window_end
 ;
