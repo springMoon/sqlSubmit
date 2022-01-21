8168f6033f1fb0197390f13ca2987298c817a0bc
wuxu <wxchunjhyy@163.com>
2022-01-21 13:44:54 +0800

    update

--- src/main/resources/sql/release/connector/mysql/flink_cdc_day_window.sql
+++ src/main/resources/sql/release/connector/mysql/flink_cdc_day_window.sql
@@ -1,5 +1,8 @@
--- mysql cdc to print
--- creates a mysql table source
+-- cdc cumulate : not support
+-- StreamPhysicalWindowAggregate doesn't support consuming update and delete changes which is produced by node
+-- TableSourceScan(table=[[default_catalog, default_database, t_user_log]], fields=[id, user_id, item_id, category_id, behavior, ts])
+
+-- cdc source
 CREATE TABLE t_user_log (
   id bigint
   ,user_id bigint
@@ -8,6 +11,7 @@ CREATE TABLE t_user_log (
   ,behavior varchar
   ,ts timestamp(3)
   ,proc_time as PROCTIME()
+  ,watermark for ts as ts  - INTERVAL '5' second
   ,PRIMARY KEY (id) NOT ENFORCED
 ) WITH (
  'connector' = 'mysql-cdc'
@@ -18,22 +22,21 @@ CREATE TABLE t_user_log (
  ,'database-name' = 'venn'
  ,'table-name' = 'user_log'
  ,'server-id' = '12345678'
+ ,'scan.startup.mode' = 'latest-offset'
 --  ,'scan.startup.mode' = 'initial'
- ,'scan.startup.mode' = 'specific-offset'
- ,'scan.startup.specific-offset.file' = 'mysql-bin.000001'
- ,'scan.startup.specific-offset.pos' = '1'
+--  ,'scan.startup.mode' = 'specific-offset'
+--  ,'scan.startup.specific-offset.file' = 'mysql-bin.000001'
+--  ,'scan.startup.specific-offset.pos' = '1'
 );
 
 -- kafka sink
 drop table if exists t_user_log_sink;
 CREATE TABLE t_user_log_sink (
-  id bigint
-  ,user_id bigint
-  ,item_id bigint
-  ,category_id bigint
-  ,behavior varchar
-  ,ts timestamp(3)
-  ,PRIMARY KEY (id) NOT ENFORCED
+  window_start string
+  ,window_end string
+  ,pv bigint
+  ,uv bigint
+--   ,PRIMARY KEY (id) NOT ENFORCED
 ) WITH (
     'connector' = 'print'
 --    'connector' = 'upsert-kafka'
@@ -48,8 +51,10 @@ CREATE TABLE t_user_log_sink (
 );
 
 insert into t_user_log_sink
-select id, user_id, item_id, category_id, behavior, ts
-from t_user_log;
-
-
-
+select date_format(window_start, 'HH:mm:ss')
+ , date_format(window_end, 'HH:mm:ss')
+ , count(id)
+ , count(distinct id)
+from TABLE(
+    cumulate(TABLE t_user_log, DESCRIPTOR(proc_time), INTERVAL '1' MINUTE, INTERVAL '1' DAY))
+group by window_start, window_end
