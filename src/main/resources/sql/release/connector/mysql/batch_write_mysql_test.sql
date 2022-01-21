4d3db27e827c41d3fac1f72bb14b8667bb3c598a
wuxu <wxchunjhyy@163.com>
2022-01-21 10:34:37 +0800

    update

--- src/main/resources/sql/release/connector/mysql/batch_write_mysql_test.sql
+++ src/main/resources/sql/release/connector/mysql/batch_write_mysql_test.sql
@@ -9,15 +9,12 @@ CREATE TABLE user_log (
   ,proc_time as proctime()
   ,WATERMARK FOR ts AS ts - INTERVAL '5' SECOND
 ) WITH (
-  'connector.type' = 'kafka'
-  ,'connector.version' = 'universal'
-  ,'connector.topic' = 'user_behavior'
-  ,'connector.properties.zookeeper.connect' = 'venn:2181'
-  ,'connector.properties.bootstrap.servers' = 'venn:9092'
-  ,'connector.properties.group.id' = 'user_log'
-  ,'connector.startup-mode' = 'group-offsets'
-  ,'connector.sink-partitioner' = 'fixed'
-  ,'format.type' = 'json'
+   'connector' = 'kafka'
+  ,'topic' = 'user_log'
+  ,'properties.bootstrap.servers' = 'localhost:9092'
+  ,'properties.group.id' = 'user_log'
+  ,'scan.startup.mode' = 'group-offsets'
+  ,'format' = 'json'
 );
 
 -- set table.sql-dialect=hive;
@@ -29,10 +26,9 @@ CREATE TABLE mysql_table_venn_user_log_sink (
   ,category_id STRING
   ,behavior STRING
   ,ts timestamp(3)
-  ,create_time timestamp(3)
 ) WITH (
   'connector' = 'jdbc'
-  ,'url' = 'jdbc:mysql://venn:3306/venn'
+  ,'url' = 'jdbc:mysql://localhost:3306/venn'
   ,'table-name' = 'user_log'
   ,'username' = 'root'
   ,'password' = '123456'
@@ -44,5 +40,5 @@ CREATE TABLE mysql_table_venn_user_log_sink (
 
 -- streaming sql, insert into mysql table
 insert into mysql_table_venn_user_log_sink
-SELECT user_id, item_id, category_id, behavior, ts, proc_time
+SELECT user_id, item_id, category_id, behavior, ts
 FROM user_log;
