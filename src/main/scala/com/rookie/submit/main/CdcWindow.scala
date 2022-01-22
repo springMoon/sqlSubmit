1f761e85635e44fe5bcc7ee13b5c92fe0759133c
wuxu <wxchunjhyy@163.com>
2022-01-22 21:56:55 +0800

    fill cdc window

--- src/main/scala/com/rookie/submit/main/CdcWindow.scala
+++ src/main/scala/com/rookie/submit/main/CdcWindow.scala
@@ -1,7 +1,11 @@
 package com.rookie.submit.main
 
-import com.ververica.cdc.connectors.mysql.MySqlSource
+import com.ververica.cdc.connectors.mysql.source.MySqlSource
+import com.ververica.cdc.connectors.mysql.table.StartupOptions
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema
+import org.apache.flink.api.common.eventtime.WatermarkStrategy
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.api.scala._
 
 /**
  * flink 1.13
@@ -12,9 +16,28 @@ object CdcWindow {
   def main(args: Array[String]): Unit = {
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
 
     val cdc = MySqlSource
       .builder[String]()
+      .hostname("localhost")
+      .port(3306)
+      .databaseList("venn")
+      .tableList("user_log")
+      .username("root")
+      .password("123456")
+      .deserializer(new JsonDebeziumDeserializationSchema())
+      .startupOptions(StartupOptions.latest())
+      .build()
+
+    env
+      .fromSource(cdc, WatermarkStrategy.noWatermarks(), "cdcSource")
+      .map(str => str)
+      .print()
+
+
+    env.execute("cdcWindow")
+
 
   }
 
